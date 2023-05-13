2305595c6b6ed25410522857d6d26b17e1c07324
ankit jodhani <ankitjodhani1903@gmail.com>
2023-05-13 20:03:20 +0530

    own cors setup and edit some of the word

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ dotenv.config();
 
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://share.ankitjodhani.club'); // Allow requests from this specific origin
+    res.setHeader('Access-Control-Allow-Origin', 'share.ankitjodhani.club'); // Allow requests from this specific origin
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); // Allow specific HTTP methods
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type'); // Allow specific headers
     next();
